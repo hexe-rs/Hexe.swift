340c7934af55544b2da8bf2cd97a50f60f70e8ce
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-18 12:52:45 -0500

    Make hexec for macOS a fat binary

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,9 +4,10 @@ CARGO="$HOME/.cargo/bin/cargo"
 
 if [[ "$PLATFORM_NAME" =~ .*macos.* ]]; then
     # macOS
-    $CARGO build --release --target "x86_64-apple-darwin"
+    TARGETS="x86_64-apple-darwin,i686-apple-darwin"
 else
     # iOS
     TARGETS="aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,i386-apple-ios,x86_64-apple-ios"
-    $CARGO lipo --release --targets $TARGETS
 fi
+
+$CARGO lipo --release --targets $TARGETS
