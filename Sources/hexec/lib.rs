d4fa2bbdb23466961395a820edb455bcd25dfe8c
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-18 00:56:05 -0500

    Pass box pointers around directly

--- Sources/hexec/lib.rs
+++ Sources/hexec/lib.rs
@@ -3,11 +3,11 @@ extern crate hexe;
 use hexe::engine::Engine;
 
 #[no_mangle]
-pub extern "C" fn hexe_engine_new() -> *mut Engine {
-    Box::into_raw(Box::new(Engine::default()))
+pub extern "C" fn hexe_engine_new() -> Box<Engine> {
+    Default::default()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn hexe_engine_destroy(e: *mut Engine) {
-    drop(Box::from_raw(e));
+pub unsafe extern "C" fn hexe_engine_destroy(e: Box<Engine>) {
+    drop(e);
 }
