a5771592efa55bd0345c9706bb3ef600b4b73490
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-18 03:12:21 -0500

    Create Square enum with most attacks methods

--- Sources/hexec/include/hexec.h
+++ Sources/hexec/include/hexec.h
@@ -1,4 +1,16 @@
+// Note: using uint8_t to represent Squares is very unsafe
+
+#include <stdint.h>
+
+typedef uint64_t Bitboard;
+
 typedef void* _Nonnull EnginePtr;
+
+Bitboard hexe_knight_attacks(uint8_t square);
+Bitboard hexe_king_attacks(uint8_t square);
+Bitboard hexe_rook_attacks(uint8_t square, Bitboard occ);
+Bitboard hexe_bishop_attacks(uint8_t square, Bitboard occ);
+Bitboard hexe_queen_attacks(uint8_t square, Bitboard occ);
 
 EnginePtr hexe_engine_new();
 void hexe_engine_destroy(EnginePtr ptr);
--- Sources/hexec/lib.rs
+++ Sources/hexec/lib.rs
@@ -1,6 +1,34 @@
 extern crate hexe;
 
+use std::mem;
+
 use hexe::engine::Engine;
+use hexe::prelude::*;
+
+#[no_mangle]
+pub unsafe extern "C" fn hexe_knight_attacks(square: u8) -> u64 {
+    Square::knight_attacks(mem::transmute(square)).0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn hexe_king_attacks(square: u8) -> u64 {
+    Square::king_attacks(mem::transmute(square)).0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn hexe_rook_attacks(square: u8, occ: u64) -> u64 {
+    Square::rook_attacks(mem::transmute(square), occ.into()).0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn hexe_bishop_attacks(square: u8, occ: u64) -> u64 {
+    Square::bishop_attacks(mem::transmute(square), occ.into()).0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn hexe_queen_attacks(square: u8, occ: u64) -> u64 {
+    Square::queen_attacks(mem::transmute(square), occ.into()).0
+}
 
 #[no_mangle]
 pub extern "C" fn hexe_engine_new() -> Box<Engine> {
