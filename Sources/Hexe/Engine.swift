57f9c77c3f4f28a59acc0d7f9f2433a38297c5bf
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-19 01:03:55 -0500

    Add documentation to Engine

--- Sources/Hexe/Engine.swift
+++ Sources/Hexe/Engine.swift
@@ -8,9 +8,12 @@
 
 import hexec
 
+/// An instance of the Hexe chess engine.
 public final class Engine {
+    /// A pointer to the engine, represented as `Box<Engine>` in Rustland
     let ptr: engine_ptr
 
+    /// Creates a new instance.
     public init() {
         ptr = hexe_engine_new()
     }
