6b16de2767a81630b8e9f178d1d6ad94addb6106
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-18 23:27:37 -0500

    Move PieceMap to a file within a Piece folder

---
+++ Sources/Hexe/Piece/Map.swift
@@ -0,0 +1,29 @@
+//
+//  Map.swift
+//  Hexe
+//
+//  Created by Nikolai Vazquez on 2/18/18.
+//  Copyright © 2018 Nikolai Vazquez. All rights reserved.
+//
+
+import hexec
+
+func memEq<T>(_ lhs: UnsafePointer<T>, _ rhs: UnsafePointer<T>) -> Bool {
+    return Int(bitPattern: lhs) == Int(bitPattern: rhs)
+        || memcmp(lhs, rhs, MemoryLayout<T>.size) == 0
+}
+
+/// A mapping of sixty-four squares to pieces.
+public final class PieceMap: Equatable {
+    /// Returns whether the piece maps are equal.
+    public static func == (lhs: PieceMap, rhs: PieceMap) -> Bool {
+        return memEq(&lhs.inner, &rhs.inner)
+    }
+
+    private var inner: piece_map
+
+    /// Creates a new instance.
+    public init() {
+        self.inner = hexe_piece_map_new()
+    }
+}
--- Sources/Hexe/Piece/Piece.swift
+++ Sources/Hexe/Piece/Piece.swift
@@ -8,11 +8,6 @@
 
 import hexec
 
-func memEq<T>(_ lhs: UnsafePointer<T>, _ rhs: UnsafePointer<T>) -> Bool {
-    return Int(bitPattern: lhs) == Int(bitPattern: rhs)
-        || memcmp(lhs, rhs, MemoryLayout<T>.size) == 0
-}
-
 /// A chess piece with a kind and color.
 public enum Piece: UInt8 {
     /// White Pawn piece.
@@ -75,20 +70,5 @@ extension Piece {
         set {
             self = unsafeBitCast((self.rawValue & ~1) | newValue.rawValue, to: Piece.self)
         }
-    }
-}
-
-/// A mapping of sixty-four squares to pieces.
-public final class PieceMap: Equatable {
-    /// Returns whether the piece maps are equal.
-    public static func == (lhs: PieceMap, rhs: PieceMap) -> Bool {
-        return memEq(&lhs.inner, &rhs.inner)
-    }
-
-    private var inner: piece_map
-
-    /// Creates a new instance.
-    public init() {
-        self.inner = hexe_piece_map_new()
     }
 }
