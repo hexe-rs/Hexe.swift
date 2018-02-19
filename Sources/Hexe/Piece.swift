23357c7f4eb882cb57cadf7ad5883b6a0c9b8418
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-18 23:04:23 -0500

    Create a Piece type with init(scalar:)

--- Sources/Hexe/Piece.swift
+++ Sources/Hexe/Piece.swift
@@ -13,6 +13,61 @@ func memEq<T>(_ lhs: UnsafePointer<T>, _ rhs: UnsafePointer<T>) -> Bool {
         || memcmp(lhs, rhs, MemoryLayout<T>.size) == 0
 }
 
+/// A chess piece with a kind and color.
+public enum Piece: UInt8 {
+    /// White Pawn piece.
+    case whitePawn
+
+    /// Black Pawn piece.
+    case blackPawn
+
+    /// White Knight piece.
+    case whiteKnight
+
+    /// Black Knight piece.
+    case blackKnight
+
+    /// White Bishop piece.
+    case whiteBishop
+
+    /// Black Bishop piece.
+    case blackBishop
+
+    /// White Rook piece.
+    case whiteRook
+
+    /// Black Rook piece.
+    case blackRook
+
+    /// White Queen piece.
+    case whiteQueen
+
+    /// Black Queen piece.
+    case blackQueen
+
+    /// White King piece.
+    case whiteKing
+
+    /// Black King piece.
+    case blackKing
+}
+
+extension Piece {
+    /// Creates a color from the case-insensitive scalar.
+    public init?(scalar: UnicodeScalar) {
+        switch scalar {
+        case "P": self = .whitePawn   case "p": self = .blackPawn
+        case "N": self = .whiteKnight case "n": self = .blackKnight
+        case "B": self = .whiteBishop case "b": self = .blackBishop
+        case "R": self = .whiteRook   case "r": self = .blackRook
+        case "Q": self = .whiteQueen  case "q": self = .blackQueen
+        case "K": self = .whiteKing   case "k": self = .blackKing
+        default:
+            return nil
+        }
+    }
+}
+
 /// A mapping of sixty-four squares to pieces.
 public final class PieceMap: Equatable {
     /// Returns whether the piece maps are equal.
