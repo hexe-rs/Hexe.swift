bbd4a988dd802db75db35725072c04827affdbd8
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-06 12:49:32 -0500

    Make enum declarations smaller by prefixing docs

--- Sources/Hexe/Piece/Piece.swift
+++ Sources/Hexe/Piece/Piece.swift
@@ -10,41 +10,18 @@ import hexec
 
 /// A chess piece with a kind and color.
 public enum Piece: UInt8 {
-    /// White Pawn piece.
-    case whitePawn
-
-    /// Black Pawn piece.
-    case blackPawn
-
-    /// White Knight piece.
-    case whiteKnight
-
-    /// Black Knight piece.
-    case blackKnight
-
-    /// White Bishop piece.
-    case whiteBishop
-
-    /// Black Bishop piece.
-    case blackBishop
-
-    /// White Rook piece.
-    case whiteRook
-
-    /// Black Rook piece.
-    case blackRook
-
-    /// White Queen piece.
-    case whiteQueen
-
-    /// Black Queen piece.
-    case blackQueen
-
-    /// White King piece.
-    case whiteKing
-
-    /// Black King piece.
-    case blackKing
+    /** White Pawn piece.   */ case whitePawn
+    /** Black Pawn piece.   */ case blackPawn
+    /** White Knight piece. */ case whiteKnight
+    /** Black Knight piece. */ case blackKnight
+    /** White Bishop piece. */ case whiteBishop
+    /** Black Bishop piece. */ case blackBishop
+    /** White Rook piece.   */ case whiteRook
+    /** Black Rook piece.   */ case blackRook
+    /** White Queen piece.  */ case whiteQueen
+    /** Black Queen piece.  */ case blackQueen
+    /** White King piece.   */ case whiteKing
+    /** Black King piece.   */ case blackKing
 }
 
 extension Piece {
@@ -90,21 +67,10 @@ extension Piece {
 
 /// A chess piece kind.
 public enum PieceKind: UInt8 {
-    /// Pawn kind.
-    case pawn
-
-    /// Knight kind.
-    case knight
-
-    /// Bishop kind.
-    case bishop
-
-    /// Rook kind.
-    case rook
-
-    /// Queen kind.
-    case queen
-
-    /// King kind.
-    case king
+    /** Pawn kind.   */ case pawn
+    /** Knight kind. */ case knight
+    /** Bishop kind. */ case bishop
+    /** Rook kind.   */ case rook
+    /** Queen kind.  */ case queen
+    /** King kind.   */ case king
 }
--- Sources/Hexe/Square.swift
+++ Sources/Hexe/Square.swift
@@ -10,197 +10,70 @@ import hexec
 
 /// A chess board square.
 public enum Square: UInt8 {
-    /// A1 square.
-    case a1
-
-    /// B1 square.
-    case b1
-
-    /// C1 square.
-    case c1
-
-    /// D1 square.
-    case d1
-
-    /// E1 square.
-    case e1
-
-    /// F1 square.
-    case f1
-
-    /// G1 square.
-    case g1
-
-    /// H1 square.
-    case h1
-
-    /// A2 square.
-    case a2
-
-    /// B2 square.
-    case b2
-
-    /// C2 square.
-    case c2
-
-    /// D2 square.
-    case d2
-
-    /// E2 square.
-    case e2
-
-    /// F2 square.
-    case f2
-
-    /// G2 square.
-    case g2
-
-    /// H2 square.
-    case h2
-
-    /// A3 square.
-    case a3
-
-    /// B3 square.
-    case b3
-
-    /// C3 square.
-    case c3
-
-    /// D3 square.
-    case d3
-
-    /// E3 square.
-    case e3
-
-    /// F3 square.
-    case f3
-
-    /// G3 square.
-    case g3
-
-    /// H3 square.
-    case h3
-
-    /// A4 square.
-    case a4
-
-    /// B4 square.
-    case b4
-
-    /// C4 square.
-    case c4
-
-    /// D4 square.
-    case d4
-
-    /// E4 square.
-    case e4
-
-    /// F4 square.
-    case f4
-
-    /// G4 square.
-    case g4
-
-    /// H4 square.
-    case h4
-
-    /// A5 square.
-    case a5
-
-    /// B5 square.
-    case b5
-
-    /// C5 square.
-    case c5
-
-    /// D5 square.
-    case d5
-
-    /// E5 square.
-    case e5
-
-    /// F5 square.
-    case f5
-
-    /// G5 square.
-    case g5
-
-    /// H5 square.
-    case h5
-
-    /// A6 square.
-    case a6
-
-    /// B6 square.
-    case b6
-
-    /// C6 square.
-    case c6
-
-    /// D6 square.
-    case d6
-
-    /// E6 square.
-    case e6
-
-    /// F6 square.
-    case f6
-
-    /// G6 square.
-    case g6
-
-    /// H6 square.
-    case h6
-
-    /// A7 square.
-    case a7
-
-    /// B7 square.
-    case b7
-
-    /// C7 square.
-    case c7
-
-    /// D7 square.
-    case d7
-
-    /// E7 square.
-    case e7
-
-    /// F7 square.
-    case f7
-
-    /// G7 square.
-    case g7
-
-    /// H7 square.
-    case h7
-
-    /// A8 square.
-    case a8
-
-    /// B8 square.
-    case b8
-
-    /// C8 square.
-    case c8
-
-    /// D8 square.
-    case d8
-
-    /// E8 square.
-    case e8
-
-    /// F8 square.
-    case f8
-
-    /// G8 square.
-    case g8
-
-    /// H8 square.
-    case h8
+    /** A1 square. */ case a1
+    /** B1 square. */ case b1
+    /** C1 square. */ case c1
+    /** D1 square. */ case d1
+    /** E1 square. */ case e1
+    /** F1 square. */ case f1
+    /** G1 square. */ case g1
+    /** H1 square. */ case h1
+    /** A2 square. */ case a2
+    /** B2 square. */ case b2
+    /** C2 square. */ case c2
+    /** D2 square. */ case d2
+    /** E2 square. */ case e2
+    /** F2 square. */ case f2
+    /** G2 square. */ case g2
+    /** H2 square. */ case h2
+    /** A3 square. */ case a3
+    /** B3 square. */ case b3
+    /** C3 square. */ case c3
+    /** D3 square. */ case d3
+    /** E3 square. */ case e3
+    /** F3 square. */ case f3
+    /** G3 square. */ case g3
+    /** H3 square. */ case h3
+    /** A4 square. */ case a4
+    /** B4 square. */ case b4
+    /** C4 square. */ case c4
+    /** D4 square. */ case d4
+    /** E4 square. */ case e4
+    /** F4 square. */ case f4
+    /** G4 square. */ case g4
+    /** H4 square. */ case h4
+    /** A5 square. */ case a5
+    /** B5 square. */ case b5
+    /** C5 square. */ case c5
+    /** D5 square. */ case d5
+    /** E5 square. */ case e5
+    /** F5 square. */ case f5
+    /** G5 square. */ case g5
+    /** H5 square. */ case h5
+    /** A6 square. */ case a6
+    /** B6 square. */ case b6
+    /** C6 square. */ case c6
+    /** D6 square. */ case d6
+    /** E6 square. */ case e6
+    /** F6 square. */ case f6
+    /** G6 square. */ case g6
+    /** H6 square. */ case h6
+    /** A7 square. */ case a7
+    /** B7 square. */ case b7
+    /** C7 square. */ case c7
+    /** D7 square. */ case d7
+    /** E7 square. */ case e7
+    /** F7 square. */ case f7
+    /** G7 square. */ case g7
+    /** H7 square. */ case h7
+    /** A8 square. */ case a8
+    /** B8 square. */ case b8
+    /** C8 square. */ case c8
+    /** D8 square. */ case d8
+    /** E8 square. */ case e8
+    /** F8 square. */ case f8
+    /** G8 square. */ case g8
+    /** H8 square. */ case h8
 }
 
 let fileBits: UInt8 = 7
