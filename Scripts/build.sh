8d6be01e038c7b14001538f9ab317ad8bf6e3433
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-06 00:24:25 -0500

    [Cargo] Only build release when Xcode uses Release

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,6 +2,10 @@
 
 CARGO="$HOME/.cargo/bin/cargo"
 
+if [[ "$CONFIGURATION" == "Release" ]]; then
+    config="--release"
+fi
+
 if [[ "$PLATFORM_NAME" =~ .*macos.* ]]; then
     # macOS
     TARGETS="x86_64-apple-darwin,i686-apple-darwin"
@@ -10,4 +14,4 @@ else
     TARGETS="aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,i386-apple-ios,x86_64-apple-ios"
 fi
 
-$CARGO lipo --release --targets $TARGETS
+$CARGO lipo $config --targets $TARGETS
