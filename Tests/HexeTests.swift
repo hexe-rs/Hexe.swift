e1e62f9d7bc8d3283d58bf27175671c466aef6f9
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-06 01:06:25 -0500

    Add tests targets

---
+++ Tests/HexeTests.swift
@@ -0,0 +1,13 @@
+//
+//  HexeTests.swift
+//  HexeTests
+//
+//  Created by Nikolai Vazquez on 3/6/18.
+//  Copyright © 2018 Nikolai Vazquez. All rights reserved.
+//
+
+import XCTest
+
+class HexeTests: XCTestCase {
+
+}
